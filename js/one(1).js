d822f13f69453b0e2ecb5166e90cbdd474c03c57
ujs12 <2839209185@qq.com>
2024-06-10 21:38:40 +0800

    上传
    上传

--- js/one(1).js
+++ js/one(1).js
@@ -409,3 +409,12 @@ zlImg.onclick = function () {
   nine.style.display = "";
   window.location.reload();
 }
+fxImg.onclick = function(){
+  if(confirm("您确定要关闭本页吗？")){
+    window.opener=null;
+    window.open('','_self');
+    window.close();
+}
+else{
+}
+}
